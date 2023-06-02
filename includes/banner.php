df8dba83196fc34dfb422e7f990c86ad148d5638
Paul Bond <git@paulsbond.co.uk>
2023-06-02 12:16:23 +0100

    CSS simplification

--- includes/banner.php
+++ includes/banner.php
@@ -1,21 +1,21 @@
 <header>
-    <a href="http://www.ccp4.ac.uk"><img src="images/ccp4_weblogo.png" alt="" height="80px"></a>
-    <a href="https://www.crystallography.org.uk/about/"><img src="images/bcalogo.gif" alt="" height="80px"></a>
-    <a href="https://www.york.ac.uk"><img src="images/UoY_logo.png" alt="" height="60px" /></a>
+    <div class="spaced">
+        <a href="http://www.ccp4.ac.uk"><img src="images/ccp4_weblogo.png" alt="" height="80px"></a>
+        <a href="https://www.crystallography.org.uk/about/"><img src="images/bcalogo.gif" alt="" height="80px"></a>
+        <a href="https://www.york.ac.uk"><img src="images/UoY_logo.png" alt="" height="60px" /></a>
+    </div>
+    <nav>
+        <a href="index.php">Home</a>
+        <a href="programme.php">School programme</a>
+        <a href="accommodation.php">Accommodation</a>
+        <a href="location.php">Location</a>
+        <a href="social.php">Social activities</a>
+        <a href="material.php">Course material</a>
+        <a href="edi.php">Equity and inclusion</a>
+        <a href="contact.php">Contact us</a>
+        <a href="application.php">&gt;&gt; Apply now! &lt;&lt;</a>
+    </nav>
 </header>
-
-<nav>
-    <a href="index.php">Home</a>
-    <a href="programme.php">School programme</a>
-    <a href="accommodation.php">Accommodation</a>
-    <a href="location.php">Location</a>
-    <a href="social.php">Social activities</a>
-    <a href="material.php">Course material</a>
-    <a href="edi.php">Equity and inclusion</a>
-    <a href="contact.php">Contact us</a>
-    <a href="application.php">&gt;&gt; Apply now! &lt;&lt;</a>
-</nav>
-
 <h1>
     CCP4-BCA Summer School 2023
     (University of York, UK)
