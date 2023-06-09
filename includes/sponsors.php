7c61f83ad72afd231ba87a607ae6ac724dc972e7
Paul Bond <git@paulsbond.co.uk>
2023-06-09 08:44:06 +0100

    Linking to logos

--- includes/sponsors.php
+++ includes/sponsors.php
@@ -1,5 +1,5 @@
 <p style="text-align:center; margin-bottom:0">Additional support:</p>
 <div class="spaced">
-   <a href="https://bbsrc.ac.uk"><img src="images/logos/bbsrc.png" height="54px"></a>
-   <a href="https://rigaku.com"><img src="images/logos/rigaku.png" height="50px"></a>
+   <a href="https://bbsrc.ac.uk"><img alt="BBSRC" height="54px" src="https://cdn-assets-cloud.frontify.com/s3/frontify-cloud-files-us/eyJwYXRoIjoiZnJvbnRpZnlcL2FjY291bnRzXC9iZlwvMTc5OTMxXC9wcm9qZWN0c1wvMjY4ODgwXC9hc3NldHNcLzkyXC80Nzk4OTAzXC8zYmU3MzRkYWM4YjIzODliNmQwMTE4ZTE0YmNkZjZlZS0xNjAyOTI0NzM3LnBuZyJ9:frontify:GXY4HKO84lg_3Y3XqJ7ZCRNsRSmIep7-6ClIZIX4arU?width=488"></a>
+   <a href="https://rigaku.com"><img alt="Rigaku" height="50px" src="https://www.rigaku.com/sites/default/files/Rigaku%20rgb_logo_2018.12.10_0.png"></a>
 </div>
