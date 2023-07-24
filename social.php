935126742c56841f1d5ab91c11b8d519144c0d44
Jon Agirre <jon.agirre@york.ac.uk>
2023-07-24 18:01:12 +0100

    Added social programme

--- social.php
+++ social.php
@@ -11,7 +11,42 @@
   <?PHP include("includes/banner.php"); ?>
   <main>
     <h2>Social programme</h2>
-    <p><strong style="color: red;">Watch this space!</strong></p>
+    <!--<p><strong style="color: red;">Watch this space!</strong></p>-->
+    <p>The St. Andrews summer school had a great tradition of bringing people together
+       and creating wonderful shared memories, and we aim to continue it. We have prepared 
+       an exciting social programme to keep your summer school networking experience 
+       enjoyable and productive.
+    </p>
+    <div class="spaced">
+      <div class="organiser">
+        <h2>Visit to York</h2>
+        <img src="https://www.york.ac.uk/media/study-new/lauren/museums-2.jpg"></img>
+        <p>After the first dinner, we will head into town for a visit. The historic City of York has much to offer, with its millenary city walls, streets from Viking times (e.g. Fossgate),
+           wonderful museums, the Minster, exclusive independent shops, Shambles market... 
+        </p>
+        <p>
+          <b>Saturday 19, 19:30 - 23:00.</b>
+        </p>
+      </div>
+      <div class="organiser">
+        <h2>Summer cèilidh</h2>
+        <img src="https://static.wixstatic.com/media/bbadd8_7c39e4ba56344512a525a80abcec6588~mv2.jpg/v1/fill/w_1026,h_770,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/FOH-Ceilidh-271.jpg"></img>
+        <p>A cèilidh is a traditional Scottish and Irish social event involving dancing and live folk music. The English variant of a cèilidh introduced a "caller", a musician who shows dancers the next dance move. 
+           Callers usually have a very good understanding of the rhythms involved in songs and different types UK regional dances, allowing them to pick the right tune and dance for every audience.
+        </p>
+        <p>
+          <b>Monday 21, 19:30 - 23:00 in James College Hall.</b>
+        </p>
+      </div>
+      <div class="organiser">
+        <h2>Dinner aboard the Captain James Cook</h2>
+        <img src="https://images.hornblower.com/825x591/images/vessels/hbyork/0c884706-ced1-460d-aa72-944c6b58e480.jpeg"></img>
+        <p>A treat for all you Buccaneer and Privateer users (users of other software also welcome). Formal dinner aboard this large vessel, which will take us up and down the Ouse river.</p>
+        <p>
+          <b>Thursday 24, 19:00 - 23:00, boarding 18:45 at King's Staith (a coach will take us there after the last session). </b>
+        </p>
+      </div>
+    </div>
   </main>
   <?PHP include("includes/sponsors.php"); ?>
 </body>
