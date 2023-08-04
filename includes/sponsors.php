47bfa29039fef048dbdf45d494b91288807b8a5c
Paul Bond <git@paulsbond.co.uk>
2023-08-04 10:13:28 +0100

    Thermo Fisher sponsor

--- includes/sponsors.php
+++ includes/sponsors.php
@@ -4,5 +4,6 @@
       <a href="https://bbsrc.ac.uk"><img alt="BBSRC" src="https://cdn-assets-cloud.frontify.com/s3/frontify-cloud-files-us/eyJwYXRoIjoiZnJvbnRpZnlcL2FjY291bnRzXC9iZlwvMTc5OTMxXC9wcm9qZWN0c1wvMjY4ODgwXC9hc3NldHNcLzkyXC80Nzk4OTAzXC8zYmU3MzRkYWM4YjIzODliNmQwMTE4ZTE0YmNkZjZlZS0xNjAyOTI0NzM3LnBuZyJ9:frontify:GXY4HKO84lg_3Y3XqJ7ZCRNsRSmIep7-6ClIZIX4arU?width=488"></a>
       <a href="https://rigaku.com"><img alt="Rigaku" src="https://www.rigaku.com/sites/default/files/Rigaku%20rgb_logo_2018.12.10_0.png"></a>
       <a href="https://sptlabtech.com"><img alt="SPT Labtech" src="https://www.sptlabtech.com/hubfs/raw_assets/public/SPT-Labtech-2021/SPT-Labtech-Theme-2021/images/logo.svg"></a>
+      <a href="https://thermofisher.com"><img alt="Thermo Fisher Scientific" src="https://corporate.thermofisher.com/content/experience-fragments/tfcorpsite/us/en/site/header/masterSJ/_jcr_content/root/container_433307184/container/container/image.coreimg.svg/1673990415361/thermo-fisher-scientific.svg"></a>
    </div>
 </footer>
