3012ef2b776a321acc532097a215dfe5182b2cf9
Paul Bond <git@paulsbond.co.uk>
2023-08-04 16:04:56 +0100

    Replaced map image

--- location.php
+++ location.php
@@ -5,13 +5,20 @@
   <title>CCP4-BCA Summer School 2023 (University of York, UK)</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" type="text/css" href="styles/ccp4.css" />
+  <style>
+    iframe {
+      width: 100%;
+      height: 500px;
+      max-height: 80vh;
+    }
+  </style>
 </head>
 
 <body>
   <?PHP include("includes/banner.php"); ?>
   <main>
     <h2>The University of York - Campus West</h2>
-    <img width="100%" src="images/annotated_map.png" /></th>
+    <iframe src="https://www.google.com/maps/d/u/0/embed?mid=18cO-JIAISz4uk1LBcHkYfsgPh6gidSU&ehbc=2E312F"></iframe>
     <p>
       Getting here is easy by both private and public methods of transportation.
       Please see the University of York's
