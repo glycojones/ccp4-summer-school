06e2c1592abc0d62020d43caa8643bfa8976b520
Paul Bond <git@paulsbond.co.uk>
2023-08-04 14:37:52 +0100

    HTML format programme

--- programme.php
+++ programme.php
@@ -5,12 +5,578 @@
   <title>CCP4-BCA Summer School 2023 (University of York, UK)</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" type="text/css" href="styles/ccp4.css" />
+  <style>
+    .spaced {
+      align-items: flex-start;
+    }
+
+    .spaced>div {
+      flex: auto;
+      max-width: 300px;
+    }
+
+    table {
+      border-collapse: collapse;
+      width: 100%;
+      text-align: left;
+    }
+
+    td,
+    h3 {
+      margin: 0;
+      padding: 4px 8px;
+      vertical-align: top;
+    }
+
+    tr {
+      background-color: #fff6ee;
+    }
+
+    tr.break {
+      background-color: #ffe393;
+    }
+
+    tr.social {
+      background-color: #cde1f2;
+    }
+
+    .time {
+      font-size: 0.9em;
+      width: 0;
+    }
+
+    .what {
+      font-weight: bold;
+      margin-bottom: 2px;
+    }
+
+    .who {
+      font-size: 0.9em;
+      margin-bottom: 2px;
+    }
+
+    .where {
+      font-size: 0.8em;
+      font-style: italic;
+    }
+  </style>
 </head>
 
 <body>
   <?PHP include("includes/banner.php"); ?>
-  <main>
-    <img width="100%" src="images/programme.png" />
+  <main class="spaced">
+    <div>
+      <h3>Saturday 19th</h3>
+      <table>
+        <tr>
+          <td class="time">14:00</td>
+          <td>
+            <div class="what">Introduction</div>
+            <div class="where">B/K/018</div>
+          </td>
+        </tr>
+        <tr>
+          <td class="time">14:30</td>
+          <td>
+            <div class="what">Student talks</div>
+            <div class="where">B/K/018</div>
+          </td>
+        </tr>
+        <tr class="break">
+          <td class="time">16:00</td>
+          <td>
+            <div class="what">Coffee</div>
+            <div class="where">Biology Atrium</div>
+          </td>
+        </tr>
+        <tr>
+          <td class="time">16:30</td>
+          <td>
+            <div class="what">Student talks</div>
+            <div class="where">B/K/018</div>
+          </td>
+        </tr>
+        <tr class="break">
+          <td class="time">18:00</td>
+          <td>
+            <div class="what">Dinner</div>
+            <div class="where">Derwent Dining Room</div>
+          </td>
+        </tr>
+        <tr class="social">
+          <td class="time">19:30</td>
+          <td>
+            <div class="what">Visit to York</div>
+            <div class="where">York City Centre</div>
+          </td>
+        </tr>
+      </table>
+    </div>
+    <div>
+      <h3>Sunday 20th</h3>
+      <table>
+        <tr class="break">
+          <td class="time">08:00</td>
+          <td>
+            <div class="what">Breakfast</div>
+            <div class="where">Derwent Dining Room</div>
+          </td>
+        </tr>
+        <tr>
+          <td class="time">09:00</td>
+          <td>
+            <div class="what">CCP4i2</div>
+            <div class="who">Stuart McNicholas (York)</div>
+            <div class="where">B/K/018</div>
+          </td>
+        </tr>
+        <tr class="break">
+          <td class="time">10:30</td>
+          <td>
+            <div class="what">Coffee</div>
+            <div class="where">Biology Atrium</div>
+          </td>
+        </tr>
+        <tr>
+          <td class="time">11:00</td>
+          <td>
+            <div class="what">Diffraction theory</div>
+            <div class="who">Kevin Cowtan (York)</div>
+            <div class="where">B/K/018</div>
+          </td>
+        </tr>
+        <tr class="break">
+          <td class="time">12:30</td>
+          <td>
+            <div class="what">Lunch</div>
+            <div class="where">Biology Atrium</div>
+          </td>
+        </tr>
+        <tr>
+          <td class="time">13:30</td>
+          <td>
+            <div class="what">Symmetry, diffraction experiments, how to get good data</div>
+            <div class="who">Chris Hill (York)</div>
+            <div class="where">B/K/018</div>
+          </td>
+        </tr>
+        <tr class="break">
+          <td class="time">15:30</td>
+          <td>
+            <div class="what">Coffee</div>
+            <div class="where">Biology Atrium</div>
+          </td>
+        </tr>
+        <tr>
+          <td class="time">16:00</td>
+          <td>
+            <div class="what">Cryo-EM</div>
+            <div class="who">Jamie Blaza (York)</div>
+            <div class="where">B/K/018</div>
+          </td>
+        </tr>
+        <tr class="break">
+          <td class="time">18:00</td>
+          <td>
+            <div class="what">Dinner</div>
+            <div class="where">Derwent Dining Room</div>
+          </td>
+        </tr>
+      </table>
+    </div>
+    <div>
+      <h3>Monday 21st</h3>
+      <table>
+        <tr class="break">
+          <td class="time">08:00</td>
+          <td>
+            <div class="what">Breakfast</div>
+            <div class="where">Derwent Dining Room</div>
+          </td>
+        </tr>
+        <tr>
+          <td class="time">09:00</td>
+          <td>
+            <div class="what">Crystallisation and crystal harvesting</div>
+            <div class="who">Tracey Gloster (St. Andrews)</div>
+            <div class="where">B/K/018</div>
+          </td>
+        </tr>
+        <tr class="break">
+          <td class="time">10:30</td>
+          <td>
+            <div class="what">Coffee</div>
+            <div class="where">Biology Atrium</div>
+          </td>
+        </tr>
+        <tr>
+          <td class="time">11:00</td>
+          <td>
+            <div class="what">DIALS</div>
+            <div class="who">Nick Devenish (Diamond)</div>
+            <div class="where">B/K/018</div>
+          </td>
+        </tr>
+        <tr class="break">
+          <td class="time">12:00</td>
+          <td>
+            <div class="what">Lunch</div>
+            <div class="where">James Hall</div>
+          </td>
+        </tr>
+        <tr>
+          <td class="time">12:45</td>
+          <td>
+            <div class="what">Scaling/merging lecture/workshop</div>
+            <div class="who">Johan Turkenburg (York)</div>
+            <div class="where">G/169 PC Room</div>
+          </td>
+        </tr>
+        <tr>
+          <td class="time">14:30</td>
+          <td>
+            <div class="what">Data pathologies</div>
+            <div class="who">Eleanor Dodson (York)</div>
+            <div class="where">G/169 PC Room</div>
+          </td>
+        </tr>
+        <tr class="break">
+          <td class="time">15:30</td>
+          <td>
+            <div class="what">Coffee</div>
+            <div class="where">James Hall</div>
+          </td>
+        </tr>
+        <tr>
+          <td class="time">16:00</td>
+          <td>
+            <div class="what">DIALS/DUI workshop</div>
+            <div class="who">The DIALS team (Diamond)</div>
+            <div class="where">G/169 PC Room</div>
+          </td>
+        </tr>
+        <tr class="break">
+          <td class="time">18:00</td>
+          <td>
+            <div class="what">Pizza</div>
+            <div class="where">James Hall</div>
+          </td>
+        </tr>
+        <tr class="social">
+          <td class="time">20:00</td>
+          <td>
+            <div class="what">C&egrave;ilidh</div>
+            <div class="who">The New Fox Band</div>
+            <div class="where">James Hall</div>
+          </td>
+        </tr>
+      </table>
+    </div>
+    <div>
+      <h3>Tuesday 22nd</h3>
+      <table>
+        <tr class="break">
+          <td class="time">08:00</td>
+          <td>
+            <div class="what">Breakfast</div>
+            <div class="where">Derwent Dining Room</div>
+          </td>
+        </tr>
+        <tr>
+          <td class="time">09:00</td>
+          <td>
+            <div class="what">CCP4 Cloud</div>
+            <div class="who">Maria Fando (CCP4)</div>
+            <div class="where">B/K/018</div>
+          </td>
+        </tr>
+        <tr class="break">
+          <td class="time">10:30</td>
+          <td>
+            <div class="what">Coffee</div>
+            <div class="where">Biology Atrium</div>
+          </td>
+        </tr>
+        <tr>
+          <td class="time">11:00</td>
+          <td>
+            <div class="what">NMR</div>
+            <div class="who">Michael Plevin (York)</div>
+            <div class="where">B/K/018</div>
+          </td>
+        </tr>
+        <tr class="break">
+          <td class="time">12:30</td>
+          <td>
+            <div class="what">Lunch</div>
+            <div class="where">Biology Atrium</div>
+          </td>
+        </tr>
+        <tr>
+          <td class="time">13:30</td>
+          <td>
+            <div class="what">Micro-ED</div>
+            <div class="who">Huw Jenkins (York)</div>
+            <div class="where">B/K/018</div>
+          </td>
+        </tr>
+        <tr class="break">
+          <td class="time">15:00</td>
+          <td>
+            <div class="what">Coffee</div>
+            <div class="where">Biology Atrium</div>
+          </td>
+        </tr>
+      </table>
+    </div>
+    <div>
+      <h3>Wednesday 23rd</h3>
+      <table>
+        <tr class="break">
+          <td class="time">08:00</td>
+          <td>
+            <div class="what">Breakfast</div>
+            <div class="where">Derwent Dining Room</div>
+          </td>
+        </tr>
+        <tr>
+          <td class="time">09:00</td>
+          <td>
+            <div class="what">Coot and map interpretation</div>
+            <div class="who">Rob Nicholls (MRC-LMB)</div>
+            <div class="where">B/K/018</div>
+          </td>
+        </tr>
+        <tr class="break">
+          <td class="time">10:30</td>
+          <td>
+            <div class="what">Coffee</div>
+            <div class="where">Biology Atrium</div>
+          </td>
+        </tr>
+        <tr>
+          <td class="time">11:00</td>
+          <td>
+            <div class="what">Coot workshop</div>
+            <div class="who">Paul Bond (York)</div>
+            <div class="where">G/169 PC Room</div>
+          </td>
+        </tr>
+        <tr class="break">
+          <td class="time">12:30</td>
+          <td>
+            <div class="what">Lunch</div>
+            <div class="where">Biology Atrium</div>
+          </td>
+        </tr>
+        <tr>
+          <td class="time">13:30</td>
+          <td>
+            <div class="what">Protein model prediction</div>
+            <div class="who">Jon Agirre (York)</div>
+            <div class="where">B/K/018</div>
+          </td>
+        </tr>
+        <tr>
+          <td class="time">14:30</td>
+          <td>
+            <div class="what">Bioinformatics</div>
+            <div class="who">Genevieve Evans (EBI)</div>
+            <div class="where">B/K/018</div>
+          </td>
+        </tr>
+        <tr class="break">
+          <td class="time">15:30</td>
+          <td>
+            <div class="what">Suppliers fair</div>
+            <div class="where">Biology Atrium</div>
+          </td>
+        </tr>
+        <tr>
+          <td class="time">17:00</td>
+          <td>
+            <div class="what">Protein modifications</div>
+            <div class="who">Jon Agirre (York)</div>
+            <div class="where">B/K/018</div>
+          </td>
+        </tr>
+        <tr class="break">
+          <td class="time">18:00</td>
+          <td>
+            <div class="what">Dinner</div>
+            <div class="where">Derwent Dining Room</div>
+          </td>
+        </tr>
+        <tr>
+          <td class="time">19:30</td>
+          <td>
+            <div class="what">Structural biology in industry</div>
+            <div class="who">Tom Davies (Astex)</div>
+            <div class="where">B/K/018</div>
+          </td>
+        </tr>
+        <tr class="social">
+          <td class="time">20:30</td>
+          <td>
+            <div class="what">Drinks and nibbles</div>
+            <div class="where">Biology Atrium</div>
+          </td>
+        </tr>
+      </table>
+    </div>
+    <div>
+      <h3>Thursday 24th</h3>
+      <table>
+        <tr class="break">
+          <td class="time">08:00</td>
+          <td>
+            <div class="what">Breakfast</div>
+            <div class="where">Derwent Dining Room</div>
+          </td>
+        </tr>
+        <tr>
+          <td class="time">09:00</td>
+          <td>
+            <div class="what">Synchrotrons</div>
+            <div class="who">Martin Walsh (Diamond)</div>
+            <div class="where">B/K/018</div>
+          </td>
+        </tr>
+        <tr class="break">
+          <td class="time">10:30</td>
+          <td>
+            <div class="what">Coffee</div>
+            <div class="where">Biology Atrium</div>
+          </td>
+        </tr>
+        <tr>
+          <td class="time">11:00</td>
+          <td>
+            <div class="what">Molecular replacement</div>
+            <div class="who">Huw Jenkins (York)</div>
+            <div class="where">B/K/018</div>
+          </td>
+        </tr>
+        <tr class="break">
+          <td class="time">12:30</td>
+          <td>
+            <div class="what">Lunch</div>
+            <div class="where">Biology Atrium</div>
+          </td>
+        </tr>
+        <tr>
+          <td class="time">13:30</td>
+          <td>
+            <div class="what">Molecular replacement workshop</div>
+            <div class="who">Huw Jenkins (York)</div>
+            <div class="where">G/169 PC Room</div>
+          </td>
+        </tr>
+        <tr class="break">
+          <td class="time">15:30</td>
+          <td>
+            <div class="what">Coffee</div>
+            <div class="where">Biology Atrium</div>
+          </td>
+        </tr>
+        <tr>
+          <td class="time">16:00</td>
+          <td>
+            <div class="what">Restraint generation</div>
+            <div class="who">Julie Tucker (York)</div>
+            <div class="where">B/K/018</div>
+          </td>
+        </tr>
+        <tr>
+          <td class="time">17:00</td>
+          <td>
+            <div class="what">Serial crystallography</div>
+            <div class="who">Briony Yorke (Bradford)</div>
+            <div class="where">B/K/018</div>
+          </td>
+        </tr>
+        <tr class="social">
+          <td class="time">18:00</td>
+          <td>
+            <div class="what">River Cruise</div>
+            <div class="who">York City Cruises</div>
+            <div class="where">Captain James Cook, River Ouse</div>
+          </td>
+        </tr>
+      </table>
+    </div>
+    <div>
+      <h3>Friday 25th</h3>
+      <table>
+        <tr class="break">
+          <td class="time">08:00</td>
+          <td>
+            <div class="what">Breakfast</div>
+            <div class="where">Derwent Dining Room</div>
+          </td>
+        </tr>
+        <tr>
+          <td class="time">09:00</td>
+          <td>
+            <div class="what">Refinement</div>
+            <div class="who">Rob Nicholls (MRC-LMB)</div>
+            <div class="where">B/K/018</div>
+          </td>
+        </tr>
+        <tr class="break">
+          <td class="time">10:30</td>
+          <td>
+            <div class="what">Coffee</div>
+            <div class="where">Biology Atrium</div>
+          </td>
+        </tr>
+        <tr>
+          <td class="time">11:00</td>
+          <td>
+            <div class="what">Refinement workshop</div>
+            <div class="who">Rob Nicholls (MRC-LMB)</div>
+            <div class="where">G/169 PC Room</div>
+          </td>
+        </tr>
+        <tr class="break">
+          <td class="time">12:30</td>
+          <td>
+            <div class="what">Lunch</div>
+            <div class="where">Biology Atrium</div>
+          </td>
+        </tr>
+        <tr>
+          <td class="time">13:30</td>
+          <td>
+            <div class="what">Structure validation</div>
+            <div class="who">Jon Agirre (York)</div>
+            <div class="where">B/K/018</div>
+          </td>
+        </tr>
+        <tr>
+          <td class="time">14:30</td>
+          <td>
+            <div class="what">Deposition</div>
+            <div class="who">Genevieve Evans (EBI)</div>
+            <div class="where">B/K/018</div>
+          </td>
+        </tr>
+        <tr class="break">
+          <td class="time">15:30</td>
+          <td>
+            <div class="what">Coffee</div>
+            <div class="where">Biology Atrium</div>
+          </td>
+        </tr>
+        <tr>
+          <td class="time">16:00</td>
+          <td>
+            <div class="what">Farewell</div>
+            <div class="where">B/K/018</div>
+          </td>
+        </tr>
+      </table>
+    </div>
   </main>
   <?PHP include("includes/sponsors.php"); ?>
 </body>
